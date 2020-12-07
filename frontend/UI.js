726488713a08c9a50e1b15d7cd58bbbfdb992b99
JUANDA-cyber <jguzman65@itfip.edu.co>
2020-12-07 13:06:16 -0500

    final para entorno de produccion

--- frontend/UI.js
+++ frontend/UI.js
@@ -17,7 +17,7 @@ class UI{
                 <div class="card m-2">
                     <div class="row">
                         <div class="col-md-4">
-                            <img src="http://localhost:3000${book.imagePath}" class="img-fluid" />
+                            <img src="${book.imagePath}" class="img-fluid" />
                         </div>
                         <div class="col-md-8">
                             <div class="card-block px-2">
